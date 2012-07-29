93d3b865cc730fb628720828e1fff6ae5eba564a
Glen Langer <contao.modul@glen-langer.de>
2012-07-29 17:45:54 +0200

    modules language file

---
+++ languages/en/modules.php
@@ -0,0 +1,22 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2012 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * PHP version 5
+ * @copyright  Glen Langer 2012 
+ * @author     Glen Langer 
+ * @package    Integrity_Check 
+ * @license    LGPL 
+ * @filesource
+ */
+
+/**
+ * For Backend List
+ */
+$GLOBALS['TL_LANG']['MOD']['integrity_check'] = 'Integrity Check (CRON)';
+
+?>
