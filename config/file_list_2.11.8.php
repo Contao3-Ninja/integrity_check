d8cad14264af25e7e58dcbb8798bcb5ca4f76397
Glen Langer <contao.modul@glen-langer.de>
2013-01-10 01:24:24 +0100

    add checksum file for Contao 2.11.8

---
+++ config/file_list_2.11.8.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2013 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * PHP version 5
+ * @copyright  Glen Langer 2013
+ * @author     Glen Langer
+ * @package    Integrity_Check
+ * @license    LGPL
+ * @filesource
+ */
+
+/**
+ * Checksums for Contao 2.11.8
+ * 
+ */
+$files2check = array
+(
+    array('index.php'       , '48b7c6e1f5319af5348fc78b861f51e3', 'f5a8ccfabeb8b26d62032cfacb3e005a','2.11.8'),
+    array('cron.php'        , '961746cae6f8ac97291804faba227b41', '3a2141de9552ae0466c42d90ad23b4dc','2.11.8'),
+    array('contao/index.php', '833b4793251e248170e92d2cf1a74b0d', '9d74ad1a5a129df17268b2a45494cb1f','2.11.8'),
+    array('contao/main.php' , '5f2454e0214e1a64e08c4bbb8f32eb29', '14ae4550377d73ce1251a4a9a0a8d7a7','2.11.8')
+);
