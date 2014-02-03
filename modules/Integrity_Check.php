1903a5b548d1815f7738cfd996d7ceb88be0ded3
Glen Langer <contao.modul@glen-langer.de>
2014-02-03 21:53:11 +0100

    Checksummen fuer Contao 3.2.5

--- modules/Integrity_Check.php
+++ modules/Integrity_Check.php
@@ -45,7 +45,7 @@ class Integrity_Check extends \Frontend
     protected $cron_interval = '';
     
     
-    const latest_version = '3.2.4';
+    const latest_version = '3.2.5';
     
     /**
      * Filelist with checksums
