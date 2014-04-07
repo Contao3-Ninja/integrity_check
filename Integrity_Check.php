7f9645ce05d7eedc8113b63696a4f93fb10b8d25
Glen Langer <contao.modul@glen-langer.de>
2014-04-07 20:41:29 +0200

    Fixed #51, Checksummen fuer 2.11.17

--- Integrity_Check.php
+++ Integrity_Check.php
@@ -37,7 +37,7 @@ class Integrity_Check extends Frontend
     
     protected $cron_interval = '';
     
-    const latest_version = '2.11.16';
+    const latest_version = '2.11.17';
     
     /**
      * Filelist with checksums
