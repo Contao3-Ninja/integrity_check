04775c2a277d9b939eb137d2eef7c21c9c576ed0
Glen Langer <contao.modul@glen-langer.de>
2014-05-26 20:03:47 +0200

    Fixed #53,#54,#55 , Checksummen fuer 3.3.0

--- modules/Integrity_Check.php
+++ modules/Integrity_Check.php
@@ -45,7 +45,7 @@ class Integrity_Check extends \Frontend
     protected $cron_interval = '';
     
     
-    const latest_version = '3.2.10';
+    const latest_version = '3.3.0';
     
     /**
      * Filelist with checksums
