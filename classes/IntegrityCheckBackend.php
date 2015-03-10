b91058a76ff7921ed328a0e4645d9c8561f509db
bibo <Glen.Langer@T-Systems.com>
2015-03-10 16:44:36 +0100

    Neue Klasse fuer Backend Tests

---
+++ classes/IntegrityCheckBackend.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Contao Open Source CMS, Copyright (C) 2005-2014 Leo Feyer
+ *
+ * Contao Module "Integrity Check"
+ *
+ * @copyright  Glen Langer 2012..2015 <http://www.contao.glen-langer.de>
+ * @author     Glen Langer (BugBuster)
+ * @package    Integrity_Check
+ * @license    LGPL
+ * @filesource
+ * @see	       https://github.com/BugBuster1701/integrity_check
+ */
+
+/**
+ * Run in a custom namespace, so the class can be replaced
+ */
+namespace BugBuster\IntegrityCheck;
+
+
+/**
+ * Class IntegrityCheckBackend 
+ * 
+ * Backend Integrity check 
+ *
+ * @copyright  Glen Langer 2012..2015 <http://www.contao.glen-langer.de>
+ * @author     Glen Langer (BugBuster)
+ * @author     Leo Feyer (sourcecode parts from contao check tool)
+ * @package    Integrity_Check
+ */
+class IntegrityCheckBackend extends \System
+{
+    
+    public static function checkAll($id)
+    {
+        
+    }
+    
+    public static function checkFile($id,$file)
+    {
+        
+    }
+    
+    public static function checkFiles($id)
+    {
+        
+    }
+    
+    public static function checkContaoUpdate($id)
+    {
+        
+    }
+    
+    public static function checkInstallCount($id)
+    {
+        
+    }
+
+}
