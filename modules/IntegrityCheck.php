899890e246ff11f352e600711436855b07749e22
Glen Langer <contao.modul@glen-langer.de>
2016-09-20 20:48:31 +0200

    Fixed #95, Checksummen für 3.5.17

--- modules/IntegrityCheck.php
+++ modules/IntegrityCheck.php
@@ -47,7 +47,7 @@ class IntegrityCheck extends \Frontend
     protected $cron_interval = '';
     
     
-    const LATEST_VERSION = '3.5.16';
+    const LATEST_VERSION = '3.5.17';
     const MESSAGE_CONTAO_UPDATE = 4;
     const MESSAGE_INSTALL_COUNT = 5;
     
