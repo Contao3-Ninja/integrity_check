121f9e2d51ac7fd583c805dde16d8ab34f79f104
Glen Langer <contao.modul@glen-langer.de>
2016-11-16 21:57:23 +0100

    Fixed #98, Checksummen für 3.5.19

--- modules/IntegrityCheck.php
+++ modules/IntegrityCheck.php
@@ -47,7 +47,7 @@ class IntegrityCheck extends \Frontend
     protected $cron_interval = '';
     
     
-    const LATEST_VERSION = '3.5.18';
+    const LATEST_VERSION = '3.5.19';
     const MESSAGE_CONTAO_UPDATE = 4;
     const MESSAGE_INSTALL_COUNT = 5;
     
